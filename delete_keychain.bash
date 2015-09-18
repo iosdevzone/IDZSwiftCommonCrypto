a92ec89934a2583e80df398c7c1c564bc30065e6
iosdevzone <idz@iosdeveloperzone.com>
2015-09-18 04:26:21 -0700

    [Carthage/Xcode] First try at cerificates

---
+++ delete_keychain.bash
@@ -0,0 +1,7 @@
+    # If this environment variable is missing, we must not be running on Travis.
+    if [ -z "$KEY_PASSWORD" ]
+    then
+        return 0
+    fi
+
+    security delete-keychain "$KEYCHAIN"
