a2e41e5b3e79b46c076ed8617489e234a3070f8b
iosdevzone <idz@iosdeveloperzone.com>
2015-10-28 20:53:29 -0700

    Fix for issue #4. Also import  instead of

--- IDZSwiftCommonCrypto/IDZSwiftCommonCrypto.h
+++ IDZSwiftCommonCrypto/IDZSwiftCommonCrypto.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 iOSDeveloperZone.com. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for IDZSwiftCommonCrypto.
 FOUNDATION_EXPORT double IDZSwiftCommonCryptoVersionNumber;
